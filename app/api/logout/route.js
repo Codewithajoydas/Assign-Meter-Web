25c515e13842791f5ffb0706d4d95b59c4f1feea
codewithajoydas@gmail.com <codewithajoydas@gmail.com>
2026-03-29 22:01:33 +0530

    setting page complete

--- app/api/logout/route.js
+++ app/api/logout/route.js
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   const response = NextResponse.redirect(
-    new URL("/login", "http://localhost:3001"),
+    new URL("/login", "https://assign-meter-web.vercel.app"),
   );
 
   response.cookies.delete("accsess_token");
