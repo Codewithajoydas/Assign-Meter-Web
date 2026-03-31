bbb68487b7cbe9c20383a1a21e4c699d8215cda8
codewithajoydas@gmail.com <codewithajoydas@gmail.com>
2026-03-31 12:20:32 +0530

    file upload bug fixed

--- app/api/updatestatus/route.js
+++ app/api/updatestatus/route.js
@@ -23,7 +23,7 @@ export async function POST(req) {
     newFormData.append("file", file);
 
     // 4. Send to Express backend
-    const response = await fetch("http://localhost:3000/api/statusupdate", {
+    const response = await fetch("https://assign-meter-backend.onrender.com/api/statusupdate", {
       method: "POST",
       headers: {
         Authorization: `Bearer ${token}`,
