0341c6dcdf23306738cff3e2c763dff29d913365
codewithajoydas@gmail.com <codewithajoydas@gmail.com>
2026-04-09 10:51:08 +0530

    Download Records Button Added

--- components/DownloadAll.jsx
+++ components/DownloadAll.jsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/navigation";
 export function DownloadAll({count}) {
     const router = useRouter();
   return (
-    <button className="bg-gray-500 hover:bg-gray-700  text-sm  py-2 px-4 rounded" onClick={()=>router.push("/api/downloadall")} style={{display:"flex", justifyContent:"center", alignItems:"center", gap:10, fontSize:"12px", cursor:"pointer"}}>
+    <button className="bg-gray-500 hover:bg-gray-700 text-white text-sm  py-2 px-4 rounded" onClick={()=>router.push("/api/downloadall")} style={{display:"flex", justifyContent:"center", alignItems:"center", gap:10, fontSize:"12px", cursor:"pointer"}}>
       <DownloadIcon size={16} />Download All {count} Records
     </button>
   );
