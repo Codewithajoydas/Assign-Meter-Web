b3d4b14b6ab78286cc4f41d04d0ee8b4989e142e
Ajoy Das <codewithajoydas@gmail.com>
2026-08-01 09:26:57 +0530

    Update login API route implementation

--- app/api/login/route.jsx
+++ app/api/login/route.jsx
@@ -4,7 +4,7 @@ export async function POST(req) {
   const body = await req.json();
 
   const res = await fetch(
-    "http://localhost:9000/api/signin",
+    "https://assign-meter-backend.onrender.com/api/signin",
     {
       method: "POST",
       headers: {
